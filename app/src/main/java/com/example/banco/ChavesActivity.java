9cc9e67cd0437c0ed29159c9a94c7526ddfa99d1
VitorSilvhan <vitor.s.conceicao6@aluno.senai.br>
2024-12-12 13:20:22 -0300

    Projeto banco faculdade

--- app/src/main/java/com/example/banco/ChavesActivity.java
+++ app/src/main/java/com/example/banco/ChavesActivity.java
@@ -1,8 +1,11 @@
 package com.example.banco;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -31,5 +34,51 @@ public class ChavesActivity extends AppCompatActivity {
 
         listView.setAdapter(adapter);
 
+    }
+
+    public void remover(View view) {
+        EditText editText = findViewById(R.id.editTextRemover);
+        String idString = editText.getText().toString();
+
+        // Verifica se o campo está vazio
+        if (idString.isEmpty()) {
+            Toast.makeText(this, "Por favor, insira um ID", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        try {
+            // conversão do id para um numero inteiro
+            Integer id = Integer.parseInt(idString);
+
+            // aqui listei todas nossas chaves
+            List<Pix> listaPix = repositorioPix.listarChaves();
+
+            // procuro no banco o pix com o id digitado
+            Pix pix = null;
+            for (Pix p : listaPix) {
+                if (p.id == id) {
+                    pix = p;
+                    break;
+                }
+            }
+            // Verifica se o Pix foi encontrado em nosso banco de dados
+            if (pix == null) {
+                Toast.makeText(this, "ID não encontrado", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            // Chama o método de remoção
+            repositorioPix.removerPix(id);
+
+            // Exibe uma mensagem de sucesso
+            Toast.makeText(this, "Remoção realizada com sucesso", Toast.LENGTH_LONG).show();
+
+        } catch (NumberFormatException e) {
+            // Captura erro de formato de número
+            Toast.makeText(this, "Digite somente números", Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            // Captura qualquer outro erro imprevisto
+            Toast.makeText(this, "Erro desconhecido: " + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
     }
 }
